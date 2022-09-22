a93de503f131bcdaeddfa1cfc7402e6568b02790
rdeguzman <ruthjf.16@gmail.com>
2022-09-22 12:10:40 -0400

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+
+/* Creating routes*/
+router.get('/Home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+router.get('/About', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+router.get('/Products', function(req, res, next) {
+  res.render('index', { title: 'Products' });
+});
+router.get('/Services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+router.get('/Contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
+});
